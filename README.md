814836c99fe7375340748ff6e84ef3fa299ac48a
Adam S. Leven <25333107+addohm@users.noreply.github.com>
2025-02-04 20:15:02 -0600

    Django framework setup

--- README.md
+++ README.md
@@ -1,23 +1,4 @@
 1 - Enter the django folder
 2 - Start a django project
-3 - In the project folder (the one that contains settings.py), create a `settings` folder
-4 - In the settings folder you just created, create a `__init__.py` file
-5 - Edit the `__init__.py` file and add the following:
-```py
-import os
-'''
-Based on the containers environment variable "PIPELINE"
-selects the appropriate settings file to use
-'''
-def get_secret(secret_id, backup=None):
-    return os.getenv(secret_id, backup)
-
-
-# Keep at the end
-if get_secret('PIPELINE') == 'production':
-    from .production import *
-else:
-    from .development import *
-```
-6 - move the original settings.py folder to this folder and rename to to `development.py`
-7 - copy the `development.py` into another new file in the same directory called `production.py`
+4 - Copy the settings folder from the django directory into ./django/project/project_name/project_name/ (the one that contains settings.py)
+5 - Enter the project folder and create an app.  Don't forget to add the app to your settings.py.
--- django/settings/production.py
+++ django/settings/production.py
@@ -1,12 +1,9 @@
-
 from pathlib import Path
 from decouple import config
 import mimetypes
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
 
 mimetypes.add_type("text/css", ".css", True)
 
@@ -14,11 +11,12 @@ mimetypes.add_type("text/css", ".css", True)
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(',')])
 CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=lambda v: [s.strip() for s in v.split(',')])
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 # Application definition
 
@@ -67,11 +65,23 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': f"django.db.backends.{config("DATABASE_ENGINE")}",
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USERNAME"),
+        "PASSWORD": config("DATABASE_PASSWORD"),
+        "HOST": config("DATABASE_ADDR"),
+        "PORT": config("DATABASE_PORT"),
     }
 }
 
+# EMAIL CONFIG
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = config("EMAIL_HOST_USER")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -103,13 +113,29 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# STATICFILES_DIRS: This lists additional directories that Django's collectstatic tool 
+# should search for static files.
+STATICFILES_DIRS = [
+    f"{BASE_DIR}/_static",
+]
+
+# STATIC_ROOT: This is the absolute path to a directory where Django's collectstatic tool 
+# will gather any static files referenced in our templates. Once collected, these can then be uploaded as a group to wherever the files are to be hosted.
+STATIC_ROOT = "/project/staticfiles"
+
+# STATIC_URL: This is the base URL location from which static files will be served, 
+# for example on a CDN.
+STATIC_URL = "static/"
+
+# MEDIA_ROOT: This is the absolute path to a directory where Django will gather any user uploaded images and files
+# ex: /var/www/files/media
+MEDIA_ROOT = "/project/media"
+
+# MEDIA_URL: This is the base URL location from which static files will be served, 
+# for example on a CDN.
+MEDIA_URL = "media/"
